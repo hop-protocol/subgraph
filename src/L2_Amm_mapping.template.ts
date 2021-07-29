f7cefaace5b29918e655ebf5349ee9b2e70f2c46
Miguel Mota <hello@miguelmota.com>
2021-07-28 20:15:02 -0700

    Add AmmFees entity

--- src/L2_Amm_mapping.template.ts
+++ src/L2_Amm_mapping.template.ts
@@ -1,11 +1,17 @@
 import {
+  BigInt,
+} from "@graphprotocol/graph-ts";
+import {
   TokenSwap
 } from '../generated/HopL2Amm/L2_Amm'
 import {
-  TokenSwap as TokenSwapEntity
+  TokenSwap as TokenSwapEntity,
+  AmmFees as AmmFeesEntity,
 } from '../generated/schema'
 
 const TOKEN_SYMBOL = '{{token}}'
+const BASIS_POINTS = '4000000' // 4bps
+const FEE_DENOMINATOR = '10000000000' // 10**10
 
 export function handleTokenSwap(event: TokenSwap): void {
   let id = event.params._event.transaction.hash.toHexString()
@@ -28,4 +34,15 @@ export function handleTokenSwap(event: TokenSwap): void {
   entity.token = TOKEN_SYMBOL
 
   entity.save()
+
+  const ammFeesId = "ammFees:{{token}}"
+  let ammFeesEntity = AmmFeesEntity.load(ammFeesId)
+  if (ammFeesEntity == null) {
+    ammFeesEntity = new AmmFeesEntity(ammFeesId)
+    ammFeesEntity.amount = BigInt.fromString('0')
+  }
+
+  ammFeesEntity.amount = ammFeesEntity.amount.plus(event.params.tokensSold.times(BigInt.fromString(BASIS_POINTS)).div(BigInt.fromString(FEE_DENOMINATOR)))
+  ammFeesEntity.token = TOKEN_SYMBOL
+  ammFeesEntity.save()
 }
