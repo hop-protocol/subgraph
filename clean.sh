abb689588e270419232e32ec7b545ff3798bd054
Miguel Mota <hello@miguelmota.com>
2021-08-09 14:14:07 -0700

    Update config assets

---
+++ clean.sh
@@ -0,0 +1,14 @@
+rm -f buildfiles.sh
+
+rm -f config/mainnet_*.json
+rm -f config/xdai_*.json
+rm -f config/matic_*.json
+rm -f config/optimism_*.json
+rm -f config/arbitrum_*.json
+
+rm -f src/L1_mapping_*_*.ts
+rm -f src/L1_token_mapping_*_*.ts
+rm -f src/L2_mapping_*_*.ts
+rm -f src/L2_Amm_mapping_*_*.ts
+rm -f src/L2_token_mapping_*_*.ts
+rm -f src/token_mapping_*_*.ts
