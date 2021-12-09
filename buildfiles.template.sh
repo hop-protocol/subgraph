f7336a69e9df5e33e80d6666eb2f0b3a682587ca
Miguel Mota <hello@miguelmota.com>
2021-12-08 16:06:38 -0800

    Add docker-compose.yml

--- buildfiles.template.sh
+++ buildfiles.template.sh
@@ -28,4 +28,9 @@ npm run build
 # auth:
 # npx graph auth https://api.thegraph.com/deploy/ $ACCESS_TOKEN
 
-npx graph deploy --debug --product hosted-service --ipfs https://api.thegraph.com/ipfs/ --node https://api.thegraph.com/deploy/ "$GITHUB_ORG/{{subgraphName}}"
+#npx graph deploy --debug --product hosted-service --ipfs https://api.thegraph.com/ipfs/ --node https://api.thegraph.com/deploy/ "$GITHUB_ORG/{{subgraphName}}"
+
+# running local:
+# docker-compose up
+# npx graph create hop-protocol/hop-optimism --node http://127.0.0.1:8020
+# npx graph deploy --debug --ipfs http://localhost:5001 --node http://localhost:8020 hop-protocol/hop-optimism
